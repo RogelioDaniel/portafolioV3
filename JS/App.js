839036b24f20612dc52df838dcce7b6d02ec1663
RogelioDaniel <rogelio-daniel@hotmail.es>
2024-04-12 02:35:55 -0600

    Responsive cellphone

--- JS/App.js
+++ JS/App.js
@@ -276,27 +276,3 @@ function animate() {
   requestAnimationFrame(animate);
 }
 
-function isMobileDevice() {
-  return /Mobi|Android/i.test(navigator.userAgent);
-}
-
-function handleScroll() {
-  if (isMobileDevice()) {
-      // Lógica para scroll con celular
-  } else {
-      // Lógica para scroll con mouse
-  }
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-  const preloader = document.getElementById('preloader');
-  const loader = document.getElementById('loader');
-
-  // Ocultar el preloader una vez que la página esté completamente cargada
-  window.addEventListener('load', function() {
-      document.body.classList.add('loaded');
-      preloader.style.display = 'none';
-  });
-
-  handleScroll();
-});
